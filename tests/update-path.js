78029ac2e9d9cf9e4f6ff848be5a00f47bdb0229
Duc Tho Tran <ducthotran2010@gmail.com>
2020-02-09 12:06:41 +0700

    Update path

--- tests/update-path.js
+++ tests/update-path.js
@@ -2,6 +2,6 @@ const { updateAbsolutePath } = require('../src/update-path');
 
 const HOST_SITE = 'https://www.hotjar.com/';
 const INPUT_FILE_PATH = 'assets/hotjar.html';
-const OUTPUT_FILE_PATH = 'assets/hotjar.html';
+const OUTPUT_FILE_PATH = 'output/hotjar.html';
 
 updateAbsolutePath(HOST_SITE, INPUT_FILE_PATH, OUTPUT_FILE_PATH);
